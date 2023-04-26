030af870abcc27874cf5f65b0e1851ecf5b983e9
Joaquin Jordan <joaquin.jordan@utec.edu.pe>
2023-04-25 20:52:53 -0500

    fix: added note

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ void readFromConsole(char buffer[], int size) {
     std::string temp;
     std::cin >> temp;
     for (int i = 0; i < size; i++)
-        buffer[i] = (i < temp.size()) ? temp[i] : ' ';
+        buffer[i] = (i < temp.size()) ? temp[i] : '\0';
     buffer[size - 1] = '\0';
     std::cin.clear();
 }
@@ -25,8 +25,7 @@ struct Record {
     }
 };
 
-
-int main() {
+void test_int_index() {
     std::function<bool(int, int)> equal = [](int a, int b) {
         return a == b;
     };
@@ -40,16 +39,50 @@ int main() {
     };
 
     ExtendibleHashFile<int, Record, decltype(equal), decltype(index), decltype(hash), 3> extendibleHash{"data.dat", false, index, equal, hash};
-//    Record new_record{};
-//    readFromConsole(new_record.code, 5);
-//    readFromConsole(new_record.name, 20);
-//    std::cin >> new_record.cycle;
-//    extendibleHash.insert(new_record);
-    extendibleHash.remove(80);
-    auto result = extendibleHash.search(80);
+    //    Record new_record{};
+    //    readFromConsole(new_record.code, 5);
+    //    readFromConsole(new_record.name, 20);
+    //    std::cin >> new_record.cycle;
+    //    extendibleHash.insert(new_record);
+    //    extendibleHash.remove(80);
+    auto result = extendibleHash.search(9012);
     for (auto &record: result) {
         std::cout << record.to_string() << std::endl;
     }
+}
+
+void test_index_char() {
+    std::function<bool(char *, char *)> equal = [](char *a, char *b) {
+        return std::string(a) == std::string(b);
+    };
+
+    std::function<char *(Record &)> index = [=](Record &record) {
+        return record.code;
+    };
+    std::hash<std::string> hasher;
+    std::function<std::size_t(char *)> hash = [&hasher](char *key) {
+        return hasher(key);
+    };
+
+    ExtendibleHashFile<char *, Record, decltype(equal), decltype(index), decltype(hash), 3> extendibleHash{"data.dat", false, index, equal, hash};
+    //    Record new_record{};
+    //    readFromConsole(new_record.code, 5);
+    //    readFromConsole(new_record.name, 20);
+    //    std::cin >> new_record.cycle;
+    //    extendibleHash.insert(new_record);
+    //    extendibleHash.remove(80);
+    auto result = extendibleHash.search("1000");
+    for (auto &record: result) {
+        std::cout << record.to_string() << std::endl;
+    }
+}
+
+int main() {
+
+    // Note: Once the index has been created with a given hash function,
+    // it can only be accessed using the same hash function, because that's
+    // the way that buckets are created to begin with.
+    test_int_index();
 
     return 0;
 }
